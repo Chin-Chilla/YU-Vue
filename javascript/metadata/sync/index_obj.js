404126afeccb21e6c574dd6d28d019a69ed4e165
jiamin-lu <jiamin.luu@hhu.edu.cn>
2022-12-30 15:00:20 +0800

    去除无用的注释

--- javascript/metadata/sync/index_obj.js
+++ javascript/metadata/sync/index_obj.js
@@ -391,8 +391,6 @@ var app = new Vue({
                 // 处理全部结果
                 synInfo.mdfileidList = new Array(); //通过标记一个空的队列，来表示同步所有候选元数据
             } else {
-                //TODO ：需要完善此处的功能，来获取所有被选择的元数据行，并将mdfileID置入 synInfo.mdfileidList 中
-                //console.log("process a part of results");
                 synInfo.mdfileidList = new Array();
                 var check = $("input[name='cellChecker']:checked");//选中的复选框
                 check.each(function () {
@@ -400,7 +398,6 @@ var app = new Vue({
                     var mdfileID = row.find("td").eq(1).text();
                     synInfo.mdfileidList.push(mdfileID);
                 });
-                //console.log(synInfo.mdfileidList);
             }
             that.sync();
         },
